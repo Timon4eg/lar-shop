91dfdf005f30c935c35731b0702424a4e4ba19fc
Timon4eg <62251008+Timon4eg@users.noreply.github.com>
2021-10-30 00:33:45 +0300

    About page
    Create about.blade.php
    Route->name(about)

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, 'index'])->name('index');
+Route::get('/about', [MainController::class, 'about'])->name('about');
 
 // Auth::routes();
 
